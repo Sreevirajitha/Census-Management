d22adc9c302379e808981ba199386cca12f69994
R. Sree Virajitha <128477627+Sreevirajitha@users.noreply.github.com>
2024-06-12 11:07:24 +0530

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -72,7 +72,7 @@ public class Admin extends JFrame{
 		add(l1);
         
 		//background
-		ImageIcon bg = new ImageIcon("C:\\Users\\VEMU ABHINAV\\Downloads\\Census-Duty-Management-main\\Background images\\emblem.png");
+		ImageIcon bg = new ImageIcon("C:\\Users\\SREE VIRAJITHA\\Downloads\\Census-Duty-Management-main\\Background images\\emblem.png");
         Image i = bg.getImage();
         Image tem_img = i.getScaledInstance(150, 120, Image.SCALE_SMOOTH);
         bg = new ImageIcon(tem_img);
@@ -87,7 +87,7 @@ public class Admin extends JFrame{
         background2.setBounds(990, 5, 480, 152);
         
         
-        ImageIcon background_image = new ImageIcon("C:\\Users\\VEMU ABHINAV\\Downloads\\Census-Duty-Management-main\\Grp-17 CensusDuty Management System\\white_bg.png");
+        ImageIcon background_image = new ImageIcon("C:\\Users\\SREE VIRAJITHA\\Downloads\\Census-Duty-Management-main\\Grp-17 CensusDuty Management System\\white_bg.png");
         Image img = background_image.getImage();
         Image temp_img = img.getScaledInstance(1920,1080, Image.SCALE_SMOOTH);
         background_image = new ImageIcon(temp_img);
