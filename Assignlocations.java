3e1cf4183d2b777bcc257446c162caba97e224ad
R. Sree Virajitha <128477627+Sreevirajitha@users.noreply.github.com>
2024-06-12 11:08:25 +0530

    Update Assignlocations.java

--- Assignlocations.java
+++ Assignlocations.java
@@ -166,7 +166,7 @@ public class Assignlocations extends JFrame {
         	}
        });
         
-        /*(employee_id ,street ,city ,district ,state ,country ,pincode) Icon icon = new ImageIcon("C:\\Users\\VEMU ABHINAV\\Downloads\\Census-Duty-Management-main\\Grp-17 CensusDuty Management System\\back arrow.jpeg");*/
+        /*(employee_id ,street ,city ,district ,state ,country ,pincode) Icon icon = new ImageIcon("C:\\Users\\SREE VIRAJITHA\\Downloads\\Census-Duty-Management-main\\Grp-17 CensusDuty Management System\\back arrow.jpeg");*/
         JButton backButton = new JButton("BACK");
         
         backButton.addActionListener(new ActionListener() {
@@ -181,7 +181,7 @@ public class Assignlocations extends JFrame {
         backButton.setBackground(Color.white);
         /*backButton.setOpaque(false);*/
         
-        ImageIcon bg = new ImageIcon("C:\\Users\\VEMU ABHINAV\\Downloads\\Census-Duty-Management-main\\Background images\\emblem.png");
+        ImageIcon bg = new ImageIcon("C:\\Users\\SREE VIRAJITHA\\Downloads\\Census-Duty-Management-main\\Background images\\emblem.png");
         Image i = bg.getImage();
         Image tem_img = i.getScaledInstance(150, 120, Image.SCALE_SMOOTH);
         bg = new ImageIcon(tem_img);
@@ -196,7 +196,7 @@ public class Assignlocations extends JFrame {
         background2.setBounds(1110, 5, 480, 152);*/
         
         
-        ImageIcon background_image = new ImageIcon("C:\\Users\\VEMU ABHINAV\\Downloads\\Census-Duty-Management-main\\Grp-17 CensusDuty Management System\\white_bg.png");
+        ImageIcon background_image = new ImageIcon("C:\\Users\\SREE VIRAJITHA\\Downloads\\Census-Duty-Management-main\\Grp-17 CensusDuty Management System\\white_bg.png");
         Image img = background_image.getImage();
         Image temp_img = img.getScaledInstance(1920,1080, Image.SCALE_SMOOTH);
         background_image = new ImageIcon(temp_img);
